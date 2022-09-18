663d7bc77975534b11fc8358756210a5293daceb
Ibrahim-Ayedi <barhoum.ayedi@gmail.com>
2022-09-18 11:11:07 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -3,26 +3,27 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
-    cartItmes.classList.remove('active');
+    cartItem.classList.remove('active');
 }
+
 let searchForm = document.querySelector('.search-form');
 
-document.querySelector('search btn').onclick = () =>{
+document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
-    navbar.classList.remove('acitve');
-    cartItmes.classList.remove('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
-let cartItmes = document.querySelector('.cart-items-container');
+let cartItem = document.querySelector('.cart-items-container');
 
-document.querySelector('cart-btn').onclick = () =>{
-    cartItmes.classList.toggle('active');
-    navbar.classList.remove('acitve');
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
     searchForm.classList.remove('active');
 }
 
 window.onscroll = () =>{
-    navbar.classList.remove('acitve');
+    navbar.classList.remove('active');
     searchForm.classList.remove('active');
-    cartItmes.classList.remove('active');
+    cartItem.classList.remove('active');
 }
